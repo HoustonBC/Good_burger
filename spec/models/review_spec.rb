935dbd61be6be6193b3686f349cc99c0f1ed92b8
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-01-23 16:31:20 -0500

    updated zipcode

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Review, type: :model do
   it { should allow_value("this").for(:title) }
   it { should_not allow_value("").for(:title) }
 
-  it { should allow_value(0).for(:rating) }
+  it { should allow_value(1).for(:rating) }
   it { should_not allow_value(nil).for(:rating) }
 
   it { should allow_value("hello").for(:body) }
