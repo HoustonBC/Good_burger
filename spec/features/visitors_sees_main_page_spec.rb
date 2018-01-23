69dba6b525b0568af372775a6a803808115b57a4
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-01-23 16:45:25 -0500

    Added restaurant feature test.

--- spec/features/visitors_sees_main_page_spec.rb
+++ spec/features/visitors_sees_main_page_spec.rb
@@ -10,6 +10,24 @@ feature "visitors sees a grid of restaurants" do
     expect(page).to have_content "a"
     expect(page).to have_link "a"
 
-    clink_link "A"
+    clink_link "Add New Restaurant"
+
+    expect(page).to have_content "Add A New Restaurant"
   end
+  scenario "clicks link and is taken to show page for given restaurant" do
+    r1 = Restaurant.create(name: "a", address: "500 summer street", city: "Boston", state: "MA", zip: "86753", picture: "this.img")
+
+    visit root_path
+
+    click_link "a"
+
+    expect(page).to have_content r1.name
+    expect(page).to have_content r1.address
+    expect(page).to have_content r1.city
+    expect(page).to have_content r1.state
+    expect(page).to have_content r1.zip
+    expect(page).to have_content r1.description
+    expect(page).to have_content r1.picture
+  end
+end
 end
