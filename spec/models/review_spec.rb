282d03815e10f912c82fe4783863667c9644f2f0
Houston Charles <houstoncharles@Houstons-MacBook-Air.local>
2018-01-23 15:53:15 -0500

    Add further tests to reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { should belong_to :restaurant }
 
+  it { should allow_value("this").for(:title) }
+  it { should_not allow_value("").for(:title) }
+
   it { should allow_value(0).for(:rating) }
   it { should_not allow_value(nil).for(:rating) }
 
