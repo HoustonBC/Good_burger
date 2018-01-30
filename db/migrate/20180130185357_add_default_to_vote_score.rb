54b847ee6ce4feacb0b92f501fe168baa34a07c7
Travis Nelson <travisnelson@Traviss-MacBook-Pro.local>
2018-01-30 15:58:53 -0500

    Deleted duplicate migration

--- db/migrate/20180130185357_add_default_to_vote_score.rb
+++
@@ -1,8 +0,0 @@
-class AddDefaultToVoteScore < ActiveRecord::Migration[5.1]
-   def up
-     change_column :reviews, :vote_score, :integer, default: 0
-   end
-   def down
-     change_column :reviews, :vote_score, :integer, default: nil
-   end
- end
